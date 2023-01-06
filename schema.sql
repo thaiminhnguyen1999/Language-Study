fff42c44485ce78f1c7f4caf18560b8b66a0514d
Nguyễn Thái Minh <nguyenthaiminh0973200965@gmail.com>
2023-01-06 19:49:44 +0700

    Update files

---
+++ schema.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE languagedev-study_db1,
+CREATE TABLE [IF NOT EXISTS] Login (
+    Name varchar(255) NOT NULL,
+    Username varchar(255) NOT NULL,
+    Password varchar(255) NOT NULL,
+    Email varchar(255),
+),
